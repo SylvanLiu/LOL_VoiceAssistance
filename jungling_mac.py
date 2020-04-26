d09309690665237bb39b345ba917c93dfacab5a4
刘思源 <58311561+SylvanLiu@users.noreply.github.com>
2020-04-27 00:31:13 +0100

    Update jungling_mac.py

--- jungling_mac.py
+++ jungling_mac.py
@@ -24,13 +24,13 @@ def init_time(start_time):
     global goal_queue
     time_queue = [red_buff, blue_buff, red_crab, blue_crab,
                   small_dragon, rift_herald, top_6, mid_6, bot_6]
-    goal_queue = ['红buff', '蓝buff', '红区河道蟹',
-                  '蓝区河道蟹', '小龙', '先锋', '上路到6', '中路到6', '下路到6']
+    goal_queue = ['red buff', 'blue buff', 'red crab',
+                  'blue crab', 'dragon', 'pioneer', 'top get 6', 'mid get 6', 'bot get 6']
     sort_queue()
 
 
 def sort_queue():
-    # 冒泡算法, 按照时间大小整理播报队列
+    # Bubble sorting algorithm, sorting the broadcasting list according the time order.
     a = 1
     global time_queue
     global goal_queue
@@ -71,12 +71,12 @@ def broadcast_queue():
                 right_boundary = current_time + datetime.timedelta(seconds=1)
                 check_point_1 = time_ - datetime.timedelta(minutes=1)
                 if left_boundary.__lt__(check_point_1) and right_boundary.__gt__(check_point_1):
-                    do_broadcast(goal_queue[i], '还有一分钟')
+                    do_broadcast(goal_queue[i], 'only one minute')
                 ckeck_point_2 = time_ - datetime.timedelta(minutes=2)
                 if left_boundary.__lt__(ckeck_point_2) and right_boundary.__gt__(ckeck_point_2):
-                    do_broadcast(goal_queue[i], '还有两分钟')
+                    do_broadcast(goal_queue[i], 'still two minutes')
                 if left_boundary.__lt__(time_) and right_boundary.__gt__(time_):
-                    do_broadcast(goal_queue[i], '已经出现')
+                    do_broadcast(goal_queue[i], 'has appeared')
                 i += 1
             time.sleep(2)
         else:
@@ -84,57 +84,60 @@ def broadcast_queue():
 
 
 def do_broadcast(goal, type_):
-    if type_ == '已经出现':
+    if type_ == 'has appeared':
         print(goal + type_)
         os.system('say ' + goal + type_)
-    else:
-        print('距离' + goal + type_)
-        os.system('say 距离' + goal + type_)
+    elif type_ == 'still two minutes':
+        print('There are ' type_ + 'before ' + goal)
+        os.system('say There are ' type_ + 'before ' + goal)
+    elif type_ == 'only one minute':
+        print('There is ' type_ + 'before ' + goal)
+        os.system('say There is ' type_ + 'before ' + goal)
 
 
 def on_release(key):
     if key == keyboard.Key.f8:
         global start_time
         start_time = datetime.datetime.now() - datetime.timedelta(seconds=15)
-        os.system('say ' + '已开始计时')
-        # 初始化所有条目时间
+        os.system('say ' + 'Timer started')
+        # Inisital all items
         init_time(start_time)
 
     if key == keyboard.Key.f1:
         global red_buff
         red_buff = datetime.datetime.now() + datetime.timedelta(minutes=5)
-        os.system('say ' + '己方红buff已记录')
-        update_item('红buff', red_buff)
+        os.system('say ' + 'red buff has recorded')
+        update_item('red buff', red_buff)
 
     if key == keyboard.Key.f2:
         global blue_buff
         blue_buff = datetime.datetime.now() + datetime.timedelta(minutes=5)
-        os.system('say ' + '己方蓝buff已记录')
-        update_item('蓝buff', blue_buff)
+        os.system('say ' + 'blue buff has been recorded')
+        update_item('blue buff', blue_buff)
 
     if key == keyboard.Key.f3:
         global red_crab
         red_crab = datetime.datetime.now() + datetime.timedelta(seconds=150)
-        os.system('say ' + '红区河道蟹已记录')
-        update_item('红区河道蟹', red_crab)
+        os.system('say ' + 'red area crab has been recorded')
+        update_item('red crab', red_crab)
 
     if key == keyboard.Key.f4:
         global blue_crab
         blue_crab = datetime.datetime.now() + datetime.timedelta(seconds=150)
-        os.system('say ' + '蓝区河道蟹已记录')
-        update_item('蓝区河道蟹', blue_crab)
+        os.system('say ' + 'blue area crab has been recorded')
+        update_item('blue crab', blue_crab)
 
     if key == keyboard.Key.f5:
         global small_dragon
         small_dragon = datetime.datetime.now() + datetime.timedelta(minutes=6)
-        os.system('say ' + '小龙已记录')
-        update_item('小龙', small_dragon)
+        os.system('say ' + 'dragon has been recorded')
+        update_item('dragon', small_dragon)
 
     if key == keyboard.Key.f6:
         global rift_herald
         rift_herald = datetime.datetime.now() + datetime.timedelta(minutes=6)
-        os.system('say ' + '峡谷先锋已记录')
-        update_item('先锋', rift_herald)
+        os.system('say ' + 'pioneer has been recorded')
+        update_item('pioneer', rift_herald)
 
     if key == keyboard.Key.delete:
         # 终结程序
@@ -154,9 +157,9 @@ if __name__ == '__main__':
     threads.append(t1)
     t2 = threading.Thread(target=broadcast_queue)
     threads.append(t2)
-    os.system('say ' + '打野辅助系统已开始运行, 祝您武运昌隆')
+    os.system('say ' + 'jungling support system has strated running, may you have a pentakill')
     for t in threads:
         t.start()
     for t in threads:
         t.join()
-    os.system('say ' + '打野辅助系统已停止运行')
+    os.system('say ' + 'jungling support system has terminated')
